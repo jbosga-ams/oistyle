8fac78393629c0a3aee8f0f4e3e32c46c8a3ffa1
Jorren Bosga <j.bosga@amsterdam.nl>
2021-04-26 22:24:52 +0000

    Typo

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    data_files=[("base", ["src/oistyle/base_style.json"])]
+    data_files=[("base", ["src/oistyle/base_style.json"])],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
